653ca6cd9b103e9703288ee74daac3e41abac1a1
Minol Subasinghe <159309946+MS-Subasinghe@users.noreply.github.com>
2024-08-09 17:14:50 +0530

    Update ResetPassword.jsx

--- client/src/Components/ResetPassword.jsx
+++ client/src/Components/ResetPassword.jsx
@@ -12,7 +12,8 @@ const ResetPassword = () => {
   
     const handleSubmit = (e) => {
       e.preventDefault();
-      axios.post('https://user-login-vercel.vercel.app/auth/reset-password/'+token, {password})
+      axios.post(`https://user-login-vercel.vercel.app/auth/reset-password/${token}`, { password })
+
         .then(res => {
           if (res.data.status) {
             navigate('/');
